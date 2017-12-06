3124bf1ce4de917e89141b5d5b25bd425b0919d7
VladmirB <botonv2@gmail.com>
2017-12-06 14:51:24 +0100

    add move strategy

---
+++ src/fr/utbm/tr54/strategie/Projet_main.java
@@ -0,0 +1,15 @@
+package fr.utbm.tr54.projet;
+
+public class Projet_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+				Controller c = new Controller();
+				c.runOnTrace();
+				//c.forward();
+				
+				
+	}
+
+}
