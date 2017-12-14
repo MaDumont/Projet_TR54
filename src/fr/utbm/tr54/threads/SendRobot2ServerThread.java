31c1edc0e6887a9d68c10cfedfff91e384dbd301
Marc Antoine Dumont <dumont.marcantoine13@gmail.com>
2017-12-14 12:05:14 +0100

    Add the communication part with the treads. Not tested

--- src/fr/utbm/tr54/threads/SendRobot2ServerThread.java
+++ src/fr/utbm/tr54/threads/SendRobot2ServerThread.java
@@ -1,25 +1,31 @@
 package fr.utbm.tr54.threads;
 
+import java.io.IOException;
+import java.net.SocketException;
+
 import fr.utbm.tr54.message.RobotServerMes;
+import lejos.network.BroadcastManager;
 
 public class SendRobot2ServerThread  extends Thread{
 	private RobotServerMes message;
 	private int timeWaiting;
+	private BroadcastManager broadcast;
 	
-	public SendRobot2ServerThread(RobotServerMes msg, int timeWaiting) {
+	public SendRobot2ServerThread(RobotServerMes msg, int timeWaiting, BroadcastManager broadcast) throws IOException {
+		
 		this.message = msg;
 		this.timeWaiting = timeWaiting;
+		this.broadcast=broadcast;
 	}
 	
 	
-	public void run() {
-		
-	
-		System.out.println("thread : " + message.getRobotId());
-		
+	public void run() {		
+
+		System.out.println("thread : " + message.getRobotId());		
 		try {
+			broadcast.broadcast(message.getByteBufferMessage());
 			Thread.sleep(timeWaiting);
-		} catch (InterruptedException e) {}
+		} catch (InterruptedException | SocketException e) {}
 			
 	}
 
